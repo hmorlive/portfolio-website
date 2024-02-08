2a4fb939777484e785716a7be18cf812660606be
Hazmed <mail@hazmedmoreno.com>
2024-02-08 00:57:42 -0500

    add: not found page design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,24 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Raleway } from "next/font/google";
+import "./globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
-
-const inter = Inter({ subsets: ['latin'] })
+const font = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Hazmed Moreno',
-  description: 'Personal Page - Hazmed Moreno',
-}
+  title: "Hazmed Moreno",
+  description: "Personal Page - Hazmed Moreno",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} relative flex flex-col text-zinc-600 bg-zinc-950`}>{children}</body>
+      <body
+        className={`${font.className} relative flex flex-col text-zinc-600 bg-zinc-950`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+// Not Found Page
+export default function NotFound () {
+    return (
+        <div className="bg-zinc-900 text-white  w-full h-screen m-0 p-0 flex flex-col items-center justify-center">
+            <h1 className="text-lg font-extralight"><span className="text-xl font-bold">404</span> | Not Found</h1>
+        </div>
+    )
+}
